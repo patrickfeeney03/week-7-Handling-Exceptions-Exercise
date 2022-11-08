960cc3ea1dfb7e588b33f35472932a954cc59b40
g00409592 <g00409592@atu.ie>
2022-11-08 13:45:19 +0000

    Added try and catch. Catch has the java.util.InputMismatchException.

--- src/main/java/ie/atu/week7/UsingTryCatch.java
+++ src/main/java/ie/atu/week7/UsingTryCatch.java
@@ -6,12 +6,17 @@ public class UsingTryCatch {
     public static void main(String[] args) {
         System.out.println("Please enter a number between 0 - 9 : ");
         Scanner myScan = new Scanner(System.in);
-        int myNum = myScan.nextInt();
-        if (myNum <= 9 && myNum > 0) {
-            System.out.println("You entered " + myNum);
+        try {
+            int myNum = myScan.nextInt();
+
+            if (myNum <= 9 && myNum > 0) {
+                System.out.println("You entered " + myNum);
+            } else {
+                System.out.println("Not a valid number");
+            }
         }
-        else {
-            System.out.println("Not a valid number");
+        catch(java.util.InputMismatchException e){
+            System.out.println("Error Caught: " + e);
         }
     }
 }
