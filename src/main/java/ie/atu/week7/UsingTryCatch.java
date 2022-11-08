1253414a70ac5ef6dd193320d167764d736f9799
g00409592 <g00409592@atu.ie>
2022-11-08 13:35:47 +0000

    Added basic code without try and catch. If a char is inputted the code will break.

--- src/main/java/ie/atu/week7/UsingTryCatch.java
+++ src/main/java/ie/atu/week7/UsingTryCatch.java
@@ -3,5 +3,15 @@ package ie.atu.week7;
 import java.util.Scanner;
 
 public class UsingTryCatch {
-
+    public static void main(String[] args) {
+        System.out.println("Please enter a number between 0 - 9 : ");
+        Scanner myScan = new Scanner(System.in);
+        int myNum = myScan.nextInt();
+        if (myNum <= 9 && myNum > 0) {
+            System.out.println("You entered " + myNum);
+        }
+        else {
+            System.out.println("Not a valid number");
+        }
+    }
 }
